048063d4990873c0b2a99ad211b20389c9fdb649
Võ Việt <vietvo371@gmail.com>
2024-12-04 13:02:38 +0000

    fix page Quản lý yêu cầu 1

--- pages/pageYeuCauCuuTro/jsobjects/JSObject1/JSObject1.js
+++ pages/pageYeuCauCuuTro/jsobjects/JSObject1/JSObject1.js
@@ -1,19 +1,29 @@
 export default {
-  unNhanLucMap: async () => {
-    try {
-      // Lấy dữ liệu từ findNhanCuuTros.run()
-      const NhanLuc = await findNhanCuuTros.run(); // Đảm bảo run() trả về Promise
-      console.log("Dữ liệu nhân lực:", NhanLuc);
+	unNhanLucMap: async () => {
+		try {
+			// Lấy dữ liệu từ findNhanCuuTros.run()
+			const NhanLuc = await findNhanCuuTros.run(); // Đảm bảo run() trả về Promise
+			console.log("Dữ liệu nhân lực:", NhanLuc);
 
-   
-      NhanLuc.forEach((item) => {
-       				  huyNhanLucs.run({"id":item.id_nhan_luc});	
-      });
 
-      // Trả về dữ liệu đã xử lý (nếu cần)
-      return NhanLuc;
-    } catch (error) {
-      console.error("Lỗi khi xử lý nhân lực:", error);
-    }
-  },
+			NhanLuc.forEach((item) => {
+				huyNhanLucs.run({"id":item.id_nhan_luc});	
+			});
+
+			// Trả về dữ liệu đã xử lý (nếu cần)
+			return NhanLuc;
+		} catch (error) {
+			console.error("Lỗi khi xử lý nhân lực:", error);
+		}
+	},
+	createNhanCuuTro:async ()=>{
+		const uuid = UUID.genV4();
+		await themMoi.run({id:uuid.hexNoDelim});
+		await changeTrangThaiCuuNans.run();
+		await changeNhanLucs.run();
+		await getYeuCauCuuTro.run();
+		await getNhanLucByNguonLuc.run();
+		showAlert("Điều phối thành công!", "success");
+		closeModal(Modal1.name);
+	}
 };
