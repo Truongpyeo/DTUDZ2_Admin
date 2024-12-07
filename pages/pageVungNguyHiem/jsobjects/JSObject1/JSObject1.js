c9ffedec7c317342140e5746fe97510bb5aaa757
vietvo371 <vietvo371@gmail.com>
2024-12-07 16:01:21 +0000

    fix admin

--- pages/pageVungNguyHiem/jsobjects/JSObject1/JSObject1.js
+++ pages/pageVungNguyHiem/jsobjects/JSObject1/JSObject1.js
@@ -8,23 +8,40 @@ export default {
 	},
 	async getCurrentLoction () {
 		var location = await appsmith.geolocation.getCurrentPosition()
-		appsmith.store
+		// appsmith.store
 		return {
 			'lat' : location.coords.latitude,
 			'lng' : location.coords.longitude,
 		}
 	},
-	actionCreateThongBao: async () => {
-		// Assuming `getAdmins` is a query to fetch existing admins
-		const uuid = UUID.genV4();
-		console.log(uuid);
-		const obj = {
-			id:uuid.hexNoDelim,
-		};
-		await Insert_vung_nguy_hiem1.run(obj);
-		await Find_vung_nguy_hiem1.run();
-		showAlert("Vùng Nguy hiểm báo đã được tạo thành công!", "success");
-		closeModal(modal_adduser.name);
+	actionCreateVungNguyHiem: async () => {
+		try {
+			// Lấy vị trí hiện tại
+			const location = appsmith.store.address || this.getCurrentLoction();
+			if (!inp_tenvung.text || !input_mota.text || !input_mucdo.text) {
+				showAlert("Không được để trống.", "error");
+				return;
+			}
+			const uuid = UUID.genV4();
+			const obj = {
+				id: uuid.hexNoDelim,
+				toa_do_x: location.lat,
+				toa_do_y: location.lng,
+			};
+			// Lưu vùng nguy hiểm và tải lại danh sách
+			await Insert_vung_nguy_hiem1.run(obj);
+			await Find_vung_nguy_hiem1.run();
+
+			// Thông báo thành công
+			showAlert("Vùng nguy hiểm đã được tạo thành công!", "success");
+
+			// Đóng modal
+			closeModal(modal_adduser.name);
+		} catch (error) {
+			// Xử lý lỗi
+			showAlert(`Có lỗi xảy ra: ${error.message}`, "error");
+		}
 	}
+
 
 }
